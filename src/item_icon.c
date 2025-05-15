b899a677fbafcf78b6205b09676f92cc63ad087f
HunarPG <HunarSangwanPG@gmail.com>
2025-05-15 16:48:00 +0530

    added merrp's key item wheel

--- src/item_icon.c
+++ src/item_icon.c
@@ -5,8 +5,11 @@
 #include "item.h"
 #include "item_icon.h"
 #include "malloc.h"
+#include "menu.h"
 #include "move.h"
+#include "palette.h"
 #include "sprite.h"
+#include "window.h"
 #include "constants/items.h"
 
 // EWRAM vars
@@ -121,6 +124,32 @@ u8 AddItemIconSprite(u16 tilesTag, u16 paletteTag, u16 itemId)
 
         return spriteId;
     }
+}
+
+u8 BlitItemIconToWindow(u16 itemId, u8 windowId, u16 x, u16 y, void * paletteDest) 
+{
+    if (!AllocItemIconTemporaryBuffers())
+        return 16;
+
+    LZDecompressWram(GetItemIconPic(itemId), gItemIconDecompressionBuffer);
+    CopyItemIconPicTo4x4Buffer(gItemIconDecompressionBuffer, gItemIcon4x4Buffer);
+    BlitBitmapToWindow(windowId, gItemIcon4x4Buffer, x, y, 32, 32);
+
+    // if paletteDest is nonzero, copies the decompressed palette directly into it
+    // otherwise, loads the compressed palette into the windowId's BG palette ID
+    if (paletteDest) 
+    {
+        void *buffer = malloc_and_decompress(GetItemIconPalette(itemId), NULL);
+
+        CpuFastCopy(buffer, paletteDest, PLTT_SIZE_4BPP);
+        Free(buffer);
+    } 
+    else 
+    {
+        LoadPalette(GetItemIconPalette(itemId), BG_PLTT_ID(gWindows[windowId].window.paletteNum), PLTT_SIZE_4BPP);
+    }
+    FreeItemIconTemporaryBuffers();
+    return 0;
 }
 
 u8 AddCustomItemIconSprite(const struct SpriteTemplate *customSpriteTemplate, u16 tilesTag, u16 paletteTag, u16 itemId)
