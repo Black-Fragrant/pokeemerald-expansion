926356ea4831f40bbdb2c84268cfe6ef0fa220d3
Black Fragrant <raisaddress@gmail.com>
2026-02-17 14:03:13 +1100

    PH1F primary scripting

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -54,10 +54,10 @@
 #define FLAG_NO_WHITEOUT                     0x28
 #define FLAG_NUVEMA_PH2F_IS_MESSY            0x29
 #define FLAG_NUVEMA_PH2F_GIFT                0x2A
-#define FLAG_UNUSED_0x02B    0x2B // Unused Flag
-#define FLAG_UNUSED_0x02C    0x2C // Unused Flag
-#define FLAG_UNUSED_0x02D    0x2D // Unused Flag
-#define FLAG_UNUSED_0x02E    0x2E // Unused Flag
+#define FLAG_NUVEMA_PH1F_LOOKER              0x2B
+#define FLAG_NUVEMA_PH1F_CHEREN_BIANCA       0x2C
+#define FLAG_NUVEMA_CHEREN                   0x2D // Unused Flag
+#define FLAG_NUVEMA_BIANCA                   0x2E // Unused Flag
 #define FLAG_UNUSED_0x02F    0x2F // Unused Flag
 #define FLAG_UNUSED_0x030    0x30 // Unused Flag
 #define FLAG_UNUSED_0x031    0x31 // Unused Flag
