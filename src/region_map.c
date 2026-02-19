6ea817aceb3d7957764f2dbbeea1b9d9040b4388
HunarPG <Hunarsangwanpg@gmail.com>
2026-02-19 12:21:14 +0530

    fixed more region map related things

--- src/region_map.c
+++ src/region_map.c
@@ -385,7 +385,7 @@ static const struct WindowTemplate sFlyMapWindowTemplates[] =
 {
     [WIN_MAPSEC_NAME] = {
         .bg = 0,
-        .tilemapLeft = 17,
+        .tilemapLeft = 1,
         .tilemapTop = 17,
         .width = 12,
         .height = 2,
@@ -394,7 +394,7 @@ static const struct WindowTemplate sFlyMapWindowTemplates[] =
     },
     [WIN_MAPSEC_NAME_TALL] = {
         .bg = 0,
-        .tilemapLeft = 17,
+        .tilemapLeft = 1,
         .tilemapTop = 15,
         .width = 12,
         .height = 4,
@@ -403,7 +403,7 @@ static const struct WindowTemplate sFlyMapWindowTemplates[] =
     },
     [WIN_FLY_TO_WHERE] = {
         .bg = 0,
-        .tilemapLeft = 1,
+        .tilemapLeft = 17,
         .tilemapTop = 18,
         .width = 14,
         .height = 2,
