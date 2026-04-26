e2e7bf100c2fe80c4f8c1d51e27bf8135a1c3268
Niels Mittertreiner <mittertreinerniels@gmail.com>
2026-04-26 18:38:34 +0200

    Set flag to make text go slower or faster than user text speed

--- include/text.h
+++ include/text.h
@@ -5,9 +5,11 @@
 #include "constants/characters.h"
 
 // This is to prevent the user from having a higher text speed modifier than the printing system can handle.
-STATIC_ASSERT(   TEXT_SPEED_SLOW_MODIFIER    <= 31
+STATIC_ASSERT(   TEXT_SPEED_SLOWER_MODIFIER    <= 31
+              && TEXT_SPEED_SLOW_MODIFIER  <= 31
               && TEXT_SPEED_MEDIUM_MODIFIER  <= 31
               && TEXT_SPEED_FAST_MODIFIER    <= 31
+              && TEXT_SPEED_FASTER_MODIFIER    <= 31
               && TEXT_SPEED_INSTANT_MODIFIER <= 31, TextSpeedModifiersCantGoPast31)
 
 // Given as a text speed when all the text should be
