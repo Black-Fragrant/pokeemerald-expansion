06f3e5d8bd765fefda767f332659975857de3dd5
Black Fragrant <raisaddress@gmail.com>
2026-05-03 15:11:54 +1000

    created place holder for castelia central plaza

--- src/data/tilesets/graphics.h
+++ src/data/tilesets/graphics.h
@@ -3523,3 +3523,22 @@ const u16 gTilesetPalettes_CasteliaCity4[][16] =
 };
 
 const u32 gTilesetTiles_CasteliaCity4[] = INCBIN_U32("data/tilesets/secondary/castelia_city_4/tiles.4bpp.lz");
+
+const u16 gTilesetPalettes_CasteliaCityCenter[][16] =
+{
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/castelia_city_center/palettes/12.gbapal"),
+};
+
+const u32 gTilesetTiles_CasteliaCityCenter[] = INCBIN_U32("data/tilesets/secondary/castelia_city_center/tiles.4bpp.lz");
