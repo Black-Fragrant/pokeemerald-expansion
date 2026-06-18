bcb9f32a59005302cbf8b4d8920c2af74f0b4834
Black Fragrant <raisaddress@gmail.com>
2026-06-18 22:22:35 +1000

    polishing up the gym puzzle map script

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -174,8 +174,7 @@
 #define VAR_CASTELIA_GYM_TRAP_2                          0x4098
 #define VAR_ROUTE_4_BIANCA_BATTLE                        0x4099
 #define VAR_RUSTURF_TUNNEL_STATE                         0x409A
-#define VAR_UNUSED_0x409B                                0x409B // Unused Var
-#define VAR_CASTELIA_GYM_HONEY_WALL_ENTRY                VAR_UNUSED_0x409B
+#define VAR_CASTELIA_GYM_HONEY_WALL_ENTRY                0x409B
 #define VAR_ELITE_4_STATE                                0x409C
 #define VAR_UNUSED_0x409D                                0x409D // Unused Var
 #define VAR_MOSSDEEP_SPACE_CENTER_STAIR_GUARD_STATE      0x409E
