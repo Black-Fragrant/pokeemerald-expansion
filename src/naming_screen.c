ace87660559b9ed6955aa63754f1398b67efd3cb
Riley Wilson <rileywilson@MacBook-Air-2.local>
2026-07-01 18:58:32 +1000

    Birch naming screen OAM bug fix.

--- src/naming_screen.c
+++ src/naming_screen.c
@@ -387,6 +387,7 @@ static void CreateHelperTasks(void);
 static void LoadPalettes(void);
 static void DrawBgTilemap(u8, const void *);
 static void NamingScreen_Dummy(u8, u8);
+static void NamingScreen_BWCleanup(void);
 static void DrawTextEntry(void);
 static void PrintKeyboardKeys(u8, u8);
 static void DrawKeyboardPageOnDeck(void);
@@ -698,15 +699,19 @@ static bool8 MainState_Exit(void)
 {
     if (!gPaletteFade.active)
     {
+        MainCallback returnCallback;
+
         if (sNamingScreen->templateNum == NAMING_SCREEN_PLAYER)
             SeedRngAndSetTrainerId();
+
         if (sNamingScreen->templateNum == NAMING_SCREEN_CAUGHT_MON
          && CalculatePlayerPartyCount() < PARTY_SIZE)
-            SetMainCallback2(BattleMainCB2);
+            returnCallback = BattleMainCB2;
         else
-            SetMainCallback2(sNamingScreen->returnCallback);
-        DestroyTask(FindTaskIdByFunc(Task_NamingScreen));
-        FreeAllWindowBuffers();
+            returnCallback = sNamingScreen->returnCallback;
+
+        NamingScreen_BWCleanup();
+        SetMainCallback2(returnCallback);
         FREE_AND_SET_NULL(sNamingScreen);
     }
     return FALSE;
@@ -2002,6 +2007,57 @@ static void DrawBgTilemap(u8 bg, const void *src)
 static void NamingScreen_Dummy(u8 bg, u8 page)
 {
 
+}
+
+static void NamingScreen_BWCleanup(void)
+{
+    u8 taskId;
+
+    gKeyRepeatStartDelay = sNamingScreen->keyRepeatStartDelayCopy;
+
+    ResetVHBlank();
+    ClearScheduledBgCopiesToVram();
+
+    HideBg(0);
+    HideBg(1);
+    HideBg(2);
+    HideBg(3);
+
+    FreeAllWindowBuffers();
+    UnsetBgTilemapBuffer(0);
+    UnsetBgTilemapBuffer(1);
+    UnsetBgTilemapBuffer(2);
+    UnsetBgTilemapBuffer(3);
+
+    taskId = FindTaskIdByFunc(Task_HandlePageSwapAnim);
+    if (taskId != TASK_NONE)
+        DestroyTask(taskId);
+
+    taskId = FindTaskIdByFunc(Task_HandleInput);
+    if (taskId != TASK_NONE)
+        DestroyTask(taskId);
+
+    taskId = FindTaskIdByFunc(Task_UpdateButtonFlash);
+    if (taskId != TASK_NONE)
+        DestroyTask(taskId);
+
+    taskId = FindTaskIdByFunc(Task_NamingScreen);
+    if (taskId != TASK_NONE)
+        DestroyTask(taskId);
+
+    ResetSpriteData();
+    FreeAllSpritePalettes();
+    ResetPaletteFade();
+    ResetBgsAndClearDma3BusyFlags(0);
+
+    SetGpuReg(REG_OFFSET_DISPCNT, DISPCNT_MODE_0);
+    SetGpuReg(REG_OFFSET_BLDCNT, 0);
+    SetGpuReg(REG_OFFSET_BLDALPHA, 0);
+    SetGpuReg(REG_OFFSET_BG2VOFS, 0);
+
+    DmaClearLarge16(3, (void *)VRAM, VRAM_SIZE, 0x1000);
+    DmaClear32(3, (void *)OAM, OAM_SIZE);
+    DmaClear16(3, (void *)PLTT, PLTT_SIZE);
 }
 
 static void DrawTextEntry(void)
